1067ddba7707af7ce4772c4dc579b6e621363e7c
ivanmulford <ivanmulford@gmail.com>
2025-08-22 10:08:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -84,4 +84,7 @@ def cambiar_estado(id, estado):
     return redirect('/admin')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
